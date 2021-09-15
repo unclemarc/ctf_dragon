072068a64d252e58a23aedeec18ca8edb8615dbb
Marc Richter <mrichter@redhat.com>
2021-09-15 14:55:40 -0400

    modified AP script for wlan1

--- ap_script/apconfig.sh
+++ ap_script/apconfig.sh
@@ -75,7 +75,7 @@ set -- "${POSITIONAL[@]}"
 
 AP_IP=${ARG_AP_IP:-'192.168.10.1'}
 AP_IP_BEGIN=`echo "${AP_IP}" | sed -e 's/\.[0-9]\{1,3\}$//g'`
-MAC_ADDRESS="$(cat /sys/class/net/wlan0/address)"
+MAC_ADDRESS="$(cat /sys/class/net/wlan1/address)"
 
 # Install dependencies
 sudo apt -y update
@@ -105,7 +105,7 @@ sudo bash -c 'cat > /etc/hostapd/hostapd.conf' << EOF
 ctrl_interface=/var/run/hostapd
 ctrl_interface_group=0
 interface=ap0
-driver=nl80211
+driver=rtl8192cu
 ssid=${AP_SSID}
 hw_mode=g
 channel=11
@@ -144,6 +144,7 @@ source-directory /etc/network/interfaces.d
 auto lo
 auto ap0
 auto wlan0
+auto wlan1
 iface lo inet loopback
 
 allow-hotplug ap0
@@ -155,6 +156,11 @@ iface ap0 inet static
 allow-hotplug wlan0
 iface wlan0 inet manual
     wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
+
+
+allow-hotplug wlan1
+iface wlan1 inet manual
+    wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
 iface AP1 inet dhcp
 EOF
 
@@ -162,33 +168,33 @@ EOF
 sudo bash -c 'cat > /bin/rpi-wifi.sh' << EOF
 echo 'Starting Wifi AP and client...'
 sleep 30
-sudo ifdown --force wlan0
+sudo ifdown --force wlan1
 sudo ifdown --force ap0
 sudo ifup ap0
-sudo ifup wlan0
+sudo ifup wlan1
 $([ "${NO_INTERNET-}" != "true" ] && echo "sudo sysctl -w net.ipv4.ip_forward=1")
 $([ "${NO_INTERNET-}" != "true" ] && echo "sudo iptables -t nat -A POSTROUTING -s ${AP_IP_BEGIN}.0/24 ! -d ${AP_IP_BEGIN}.0/24 -j MASQUERADE")
 $([ "${NO_INTERNET-}" != "true" ] && echo "sudo systemctl restart dnsmasq")
 EOF
 sudo chmod +x /bin/rpi-wifi.sh
 
-# Configure cron job
-# sudo bash -c 'cat > /etc/systemd/system/rpi-wifi.service' << EOF
-# [Unit]
-# Description=Simultaneous AP and Managed Mode Wifi on Raspberry Pi
-# Requires=network.target
-# After=network.target
-#
-# [Service]
-# ExecStart=/bin/bash -c 'rpi-wifi.sh'
-# User=root
-#
-# [Install]
-# WantedBy=multi-user.target
-# EOF
-# sudo systemctl daemon-reload
-# sudo systemctl enable rpi-wifi.service
-crontab -l | { cat; echo "@reboot /bin/rpi-wifi.sh"; } | crontab -
+# Configure service
+sudo bash -c 'cat > /etc/systemd/system/rpi-wifi.service' << EOF
+[Unit]
+Description=Simultaneous AP and Managed Mode Wifi on Raspberry Pi
+Requires=network.target
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c 'rpi-wifi.sh'
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable rpi-wifi.service
+#crontab -l | { cat; echo "@reboot /bin/rpi-wifi.sh"; } | crontab -
 
 # Finish
 echo "Wifi configuration is finished! Please reboot your Raspberry Pi to apply changes..."
