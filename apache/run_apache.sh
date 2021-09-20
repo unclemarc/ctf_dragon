dc25c0db134fcf0097d168bf1046dd1e5c7553d4
Marc Richter <mrichter@redhat.com>
2021-09-19 22:43:00 -0400

    apache now works

--- apache/run_apache.sh
+++ apache/run_apache.sh
@@ -3,9 +3,9 @@ docker run \
   --name=dragon-apache \
   -p 80:80 \
   -v /home/pi/lab/ctf_dragon/apache/volumes/flag/:/tmp/f \
-  -v /home/pi/lab/ctf_dragon/apache/volumes/var-www/:/var/www \
+  -v /home/pi/lab/ctf_dragon/apache/volumes/var-www/:/var/www/html \
   -v /home/pi/lab/ctf_dragon/apache/volumes/etc-apache2/:/etc/apache2 \
   -v /home/pi/lab/ctf_dragon/apache/volumes/cgi-bin:/usr/lib/cgi-bin \
-  -v /home/pi/lab/ctf_dragon/apache/volumes/log:/var/log \
+  -v /home/pi/lab/ctf_dragon/apache/volumes/log:/var/log/apache2 \
   dragon-apache
 
