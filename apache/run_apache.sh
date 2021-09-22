4b36c4543a2054751dc6b1dddc5c74ab4f4bbfa0
Marc Richter <mrichter@redhat.com>
2021-09-22 10:17:18 -0400

    PHP added and the biatch works!

--- apache/run_apache.sh
+++ apache/run_apache.sh
@@ -4,7 +4,6 @@ docker run \
   -p 80:80 \
   -v /home/pi/lab/ctf_dragon/apache/volumes/flag/:/tmp/f \
   -v /home/pi/lab/ctf_dragon/apache/volumes/var-www/:/var/www/html \
-  -v /home/pi/lab/ctf_dragon/apache/volumes/etc-apache2/:/etc/apache2 \
   -v /home/pi/lab/ctf_dragon/apache/volumes/cgi-bin:/usr/lib/cgi-bin \
   -v /home/pi/lab/ctf_dragon/apache/volumes/log:/var/log/apache2 \
   dragon-apache
---
+++ apache/volumes/var-www/step2.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+The code should be <?php echo file_get_contents("/tmp/f/f"); ?>
+</body>
+</html>
+
