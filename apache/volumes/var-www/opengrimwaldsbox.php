0f13cacf439fc29d07937bd92fc7006a6806b6ff
Marc Richter <mrichter@redhat.com>
2021-09-22 19:41:24 -0400

    web story set

---
+++ apache/volumes/var-www/opengrimwaldsbox.php
@@ -0,0 +1,4 @@
+<p>You see an <a href="/grimwald_id_rsa">iron key</a> in the box.<br />
+You see a small scroll that glows with magic light. On it you can read <?php echo file_get_contents("/tmp/f/f"); ?>&nbsp;</p>
+
+<h1>&nbsp;</h1>
