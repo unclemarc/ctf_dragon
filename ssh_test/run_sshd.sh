20673a2ea82d11bdc29b079ea7747a285ca79452
Marc Richter <mrichter@redhat.com>
2021-09-23 15:35:39 -0400

    secured flag2 for ssh challenge

--- ssh_test/run_sshd.sh
+++ ssh_test/run_sshd.sh
@@ -3,5 +3,6 @@ docker run \
   --name=dragon-ssh \
   -p 2022:22 \
   -v /home/pi/lab/ctf_dragon/ssh_test/volumes/princess:/usr/local/share/princess \
+  -v /home/pi/lab/ctf_dragon/ssh_test/volumes/flag2:/tmp/f/vault \
   dragon-ssh
 
