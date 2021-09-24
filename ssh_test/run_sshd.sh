28b6b4f84b7cc4dda43633b663c0f1563d34494a
Marc Richter <mrichter@redhat.com>
2021-09-24 10:31:08 -0400

    changed add_ips for 192.168.42 subnet

--- ssh_test/run_sshd.sh
+++ ssh_test/run_sshd.sh
@@ -1,7 +1,8 @@
 docker run \
   -d \
-  --name=dragon-ssh \
-  -p 2022:22 \
+  --name dragon-ssh \
+  --ip 192.168.42.243 \
+  -p 192.168.42.243:22:22 \
   -v /home/pi/lab/ctf_dragon/ssh_test/volumes/princess:/usr/local/share/princess \
   -v /home/pi/lab/ctf_dragon/ssh_test/volumes/flag2:/tmp/f/vault \
   dragon-ssh
