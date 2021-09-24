e2e6fc5b42139e387bd7e8dc4d8da3b7d3656882
Marc Richter <mrichter@redhat.com>
2021-09-24 10:43:41 -0400

    added startup for boris ssh

---
+++ ssh_test/boris_run_sshd.sh
@@ -0,0 +1,9 @@
+docker run \
+  -d \
+  --name dragon-ssh \
+  --ip 192.168.42.243 \
+  -p 192.168.42.243:2022:22 \
+  -v /home/pi/lab/ctf_dragon/ssh_test/volumes/princess:/usr/local/share/princess \
+  -v /home/pi/lab/ctf_dragon/ssh_test/volumes/flag2:/tmp/f/vault \
+  dragon-ssh
+
