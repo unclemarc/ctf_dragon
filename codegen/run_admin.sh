c37aa6d6bf3e969753baf62dee7535e6f5b691eb
Marc Richter <mrichter@redhat.com>
2021-09-27 12:27:45 -0400

    more tweaks on startup scripts

--- codegen/run_admin.sh
+++ codegen/run_admin.sh
@@ -3,6 +3,6 @@ docker run \
   --name codeadmin \
   --ip 192.168.42.2 \
   -p 192.168.42.2:22:22 \
-  -v /home/pi/lab/ctf_dragon/codgen:/home/admin/codegen \
+  -v /home/pi/lab/ctf_dragon/codegen:/home/admin/codegen \
   codeadmin
 
