eabe50642a3530820f7fca4178e9685e307b43f3
Marc Richter <mrichter@redhat.com>
2021-09-30 11:16:24 -0400

    added grats text for first flag

--- apache/volumes/var-www/opengrimwaldsbox.php
+++ apache/volumes/var-www/opengrimwaldsbox.php
@@ -1,4 +1,5 @@
 <p>You see an <a href="/grimwald_id_rsa">iron key</a> in the box.<br />
 You see a small scroll that glows with magic light. On it you can read <?php echo file_get_contents("/tmp/f/f"); ?>&nbsp;</p>
 
+<p>Well done! You have found the first flag out of three. Be sure to submit it to the scoreboard. Now hoist your drink and find the next one!</p> 
 <h1>&nbsp;</h1>
