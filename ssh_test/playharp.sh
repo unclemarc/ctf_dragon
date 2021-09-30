8efaf23b6e03bdc6b1b48505379e32e18d1dd715
Marc Richter <mrichter@redhat.com>
2021-09-30 13:11:46 -0400

    third challenge in and working

---
+++ ssh_test/playharp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "You pick up the harp and begin to play. "
+echo "There are flashes of multi-colored lightning from harp's body."
+echo "In the distance, there is a roar and then a terrible bellowing sound."
+echo "This is followed by an tremendous explosion."
+echo "Putting the harp down, you make your way to the roof of the castle."
+echo "There is a giant blue smoking dragon corpse that seems to have burst open."
+echo "Next to the dragon is a large chest."
+echo "Opening the chest, you see coins, gems and other treasures."
+echo "A small golden scroll floats up and on it you see a magical code."
+echo "You have defeated Sparky the blue dragon!"
+echo "You have also found your third and final flag for this part of the contest."
+echo "Well done, adventure!"
+
+
+flag=`cat /tmp/f/vault/flag`
+echo "The flag is "$flag
